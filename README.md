55d302f34df6903ee8207157f2e9f7f1055777cc
Jose Henrique Roveda <zeh.henrique92@gmail.com>
2020-10-07 19:00:25 -0300

    Updated main

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# spraying-API
-real-time analysis of climatic conditions generating the result whether they are suitable or not for agricultural spraying
+# 🌱 Spraying API 
+
+## 🚜 What is the importance of spraying in agriculture?
+
+Spraying is of fundamental importance in agriculture, being responsible for protecting crops against pests, diseases, insects and making nutrients available to plants, ensuring good productivity. This API aims to assist in the analysis of climatic conditions in real time, to inform whether these conditions are suitable or not for agricultural spraying.
+
+## 💻 Using this API:
+
+This API was built with [FastAPI](https://github.com/tiangolo/fastapi). 
+FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.6+ based on standard Python type hints.
+
+The key features are:
+* **Fast:** Very high performance, on par with NodeJS and Go (thanks to Starlette and Pydantic). One of the fastest Python frameworks available.
+* **Fast to code:** Increase the speed to develop features by about 200% to 300%. ¹
+* **Fewer bugs:** Reduce about 40% of human (developer) induced errors. ¹
+* **Intuitive:** Great editor support. Completion everywhere. Less time debugging.
+* **Easy:** Designed to be easy to use and learn. Less time reading docs.
+* **Short:** Minimize code duplication. Multiple features from each parameter declaration. Fewer bugs.
+* **Robust:** Get production-ready code. With automatic interactive documentation.
+* **Standards-based:** Based on (and fully compatible with) the open standards for APIs: OpenAPI (previously known as Swagger) and JSON Schema.
+
+*¹ estimation based on tests on an internal development team, building production applications.*
+
+
+To run it:
+
+```
+uvicorn main:app --reload
+```
+
+![](https://raw.githubusercontent.com/josehenriqueroveda/spraying-API/main/examples/im1.png)
+
+☁️ **Where did climate data come from before analysis?**
+The wheather data come from OpenWeatherMap, it is an online service, owned by OpenWeather Ltd, that provides global weather data, including current weather data, forecasts, nowcasts and historical data, utilising meteorological broadcast services and raw data from airport weather stations, radar stations and other weather stations. After receiving the meteorological data, the API performs the processing, returning the spray conditions
+
+## Examples
+
+* In this case, the city of Columbia is in good condition for spraying. 🌿
+![](https://raw.githubusercontent.com/josehenriqueroveda/spraying-API/main/examples/im4.png)
+
+* The city of Longview is not in good condition, due to the low relative humidity of the air. 🍂
+![](https://raw.githubusercontent.com/josehenriqueroveda/spraying-API/main/examples/im2.png)
+
+* Dallas is not in a good condition for agricultural spraying due to the high wind speed, which causes drift. 💨
+![](https://raw.githubusercontent.com/josehenriqueroveda/spraying-API/main/examples/im3.png)
+
+---
+
+### About:
+
+> - 💻 **José Henrique Roveda**
+> - 📨 Contact me on [LinkedIn](https://www.linkedin.com/in/jhroveda/)
+
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from fastapi import FastAPI
 from starlette.responses import RedirectResponse
 from ratelimit import limits
+import uvicorn
 import requests
 import json
 import config
 
 ONE_MINUTE = 60
 
-app = FastAPI(title='Spraying conditions API', description='API for real-time analysis of climatic conditions generating the result whether they are suitable or not for agricultural spraying.')
-
+app = FastAPI(title='Spraying conditions API',
+              description='API for real-time analysis of climatic conditions generating the result whether they are suitable or not for agricultural spraying.')
 
 
 @app.get("/")
@@ -21,8 +22,7 @@ async def docs():
 @app.get("/spray/condition")
 async def check_spray_condition(city: str):
     try:
-        response = await requests.get(
-            f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=pt&appid={config.OWM_KEY}')
+        response = requests.get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&lang=pt&appid={config.OWM_KEY}')
 
         wheather_info = json.loads(response.text)
         description = wheather_info['weather'][0]['main']
@@ -30,10 +30,11 @@ async def check_spray_condition(city: str):
         feels_like = int(wheather_info['main']['feels_like'])
         humidity = int(wheather_info['main']['humidity'])
         wind = int(wheather_info['wind']['speed'])*3.6
+        spray_condition = ''
 
         bad_conditions = ['Thunderstorm', 'Drizzle', 'Rain', 'Snow']
 
-        if description not in bad_conditions and (10 < temperature < 30) and (10 < feels_like < 30) and (humidity > 50) and (3 < wind < 10):
+        if (description not in bad_conditions) and (10 < temperature < 30) and (10 < feels_like < 30) and (humidity > 50) and (3 < wind < 10):
             spray_condition = 'Good weather conditions for spraying'
         else:
             if description in bad_conditions:
@@ -44,19 +45,24 @@ async def check_spray_condition(city: str):
                 spray_condition = f'Bad weather conditions: {temperature} °C is too cold for spraying'
             elif (temperature < 30) and (feels_like < 30) and (humidity < 50) and (3 < wind < 10):
                 spray_condition = f'Bad weather conditions: {humidity} % air humidity. It is below that recommended for spraying'
-            elif (temperature < 30) and  (feels_like < 30) and (humidity > 50) and (wind < 3):
+            elif (temperature < 30) and (feels_like < 30) and (humidity > 50) and (wind < 3):
                 spray_condition = f'Bad weather conditions: The wind speed of {wind} km/h is very low and not recommended for spraying'
             elif (temperature < 30) and (feels_like < 30) and (humidity > 50) and (wind > 10):
                 spray_condition = f'Bad weather conditions: The wind speed of {wind} km/h is above the recommended and can cause drift.'
             else:
-                spray_condition = 'Bad weather conditions for spraying'    
-
-        return {'city': city,
-                'description': description,
-                'temperature': f'{temperature} °C',
-                'feels_like': f'{feels_like} °C',
-                'humidity': f'{humidity} %',
-                'wind': f'{wind} km/h',
-                'spray_condition': spray_condition}
+                spray_condition = 'Bad weather conditions for spraying'
+        
+        
+        result = ({'city': city,
+                   'description': description,
+                   'temperature': f'{temperature} °C',
+                   'feels_like': f'{feels_like} °C',
+                   'humidity': f'{humidity} %',
+                   'wind': f'{wind} km/h',
+                   'spray_condition': spray_condition})
+        return result
     except:
         pass
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
